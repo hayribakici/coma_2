6931a15dc47bf12728c6dad63e88d1c9a827c67c
Hayri Bakici <hayri.bakici@gmail.com>
2021-04-30 00:16:20 +0200

    putting it together
    Going back to square 1.
    It looks like the lagrange function returns errornous results.
    I wanted to visualize the the progression for each n = 1, 2, 3 ...,
    however since lagrance() is errornous, we need to figure out this out,
    without the progression first.

--- ha_1.py
+++ ha_1.py
@@ -21,14 +21,16 @@ def lagrange(x, nodes, k):
     @returns: The value of the lagrange-polynome at the point x
     """
 
-    return functools.reduce(lambda a, xi: a * (x - xi) / (nodes[k] - xi), nodes)
+    # return functools.reduce(lambda a, xi: a * (x - xi) / (nodes[k] - xi), nodes)
 
     # Possible different way of writing this algorithm.
-    # product = 1
-    # for node in nodes:
-    #     product *= ((x - node) / (nodes[k] - node))
+    # TODO this throws an exception when nodes[k] - node = 0
+    # looks like nodes[k] (or the x_k to be exact) is a different number
+    product = 1
+    for node in nodes:
+        product *= ((x - node) / (nodes[k] - node))
     
-    # return product
+    return product
 
 
 # Aufgabe 3 b
@@ -40,13 +42,13 @@ def lagrange_interpolation(x, nodes, function_values):
     # we want to make sure our algorithm meets this condition
     assert len(nodes) == len(function_values)
     
-    return functools.reduce(lambda a, b: a + b * lagrange(x, nodes, function_values.indexof(b)), function_values)
+    # return functools.reduce(lambda a, b: a + b * lagrange(x, nodes, function_values.index(b)), function_values)
 
-    # sum = 0
-    # for k, value in enumerate(function_values):
-    #     sum += value * lagrange(x, nodes, k)
+    sum = 0
+    for k, value in enumerate(function_values):
+        sum += (value * lagrange(x, nodes, k))
     
-    # return sum
+    return sum
 
 # Aufgabe 3 c
 
@@ -54,18 +56,21 @@ def plot(n):
     """
     Plots in a grid of n rows
     """
+
+
     rowspan = 2
     colspan = 3
-    for i in range(1, (n + 1)):
-        print(str(i) + " " + str(i - 1) + " " + str((i - 1) * rowspan))
-        subplot = plt.subplot2grid(((n * 2) + 1, colspan), ((i - 1) * rowspan, 0), rowspan=rowspan, colspan=colspan)
-        x = _get_supporting_points(i)
-        y = _get_function_values(math.sin, i)
-        subplot.plot(x, y)
-        # subplot.xlabel('x')
-        # subplot.xlabel('y')
-        subplot.set_title('for n = ' + str(i))
-    plt.tight_layout()
+    x = np.arange(0, math.pi, 0.02)
+    y = map(lambda item: lagrange_interpolation(item, _get_supporting_points(n), _get_function_values(math.sin, n)), x)
+    plt.plot(x, y)
+
+    # TODO uncomment when above code workds
+    # for i in range(1, (n + 1)):
+    #     subplot = plt.subplot2grid(((n * 2) + 1, colspan), ((i - 1) * rowspan, 0), rowspan=rowspan, colspan=colspan)
+    #     y = map(lambda item: lagrange_interpolation(item, _get_supporting_points(i), _get_function_values(math.sin, i)), x)
+    #     subplot.plot(x, y)
+    #     subplot.set_title('for n = ' + str(i))
+    # plt.tight_layout()
     plt.show()
 
 
@@ -101,9 +106,9 @@ def _get_supporting_points(n):
     """
     # since k has the range from 0 ... n, the range to n + 1 is necessary
     # otherwise it would go from 0 ... n - 1
-    list = [k for k in range(n + 1)] # equals [0, 1, 2, ..., n]
+    mylist = [k for k in range(n + 1)] # equals [0, 1, 2, ..., n]
 
-    return map(lambda item: (item * math.pi) / n, list)
+    return map(lambda item: (item * math.pi) / n, mylist)
 
     # Possible different way of the same algorithm:
 
