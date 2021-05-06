500d94b0cf51fc937780d295931e64e2653e2958
Hayri Bakici <hayri.bakici@gmail.com>
2021-05-06 22:47:17 +0200

    implements difference quotient
    - adds method signature stubs

--- ha_2/run_2_3.py
+++ ha_2/run_2_3.py
@@ -1,1 +1,15 @@
 # TODO Aufgabe 3a, b programmieren
+
+def difference_quotient(h, function, x):
+    if h == 0:
+        return 0
+    return (function(x + h) - function(x)) / h
+
+
+def aitken_neville(x):
+    # TODO implement me
+    pass
+
+def _aitken_neville(i, k, x):
+    # TODO implement recursive function
+    pass
