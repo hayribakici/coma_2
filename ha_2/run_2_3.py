77bb5449b74dc0e66bc595b14137f32724012fe5
Hayri Bakici <hayri.bakici@gmail.com>
2021-05-09 10:39:51 +0200

    first attempt for dynamic nodes

--- ha_2/run_2_3.py
+++ ha_2/run_2_3.py
@@ -17,15 +17,19 @@ def _aitken_neville(i, k, function, x, nodes):
     xk = nodes[k]
     print("i: %s, k: %s, xi: %s, xk: %s", i, k, xi, xk)
     if i == k:
-        return difference_quotient(xi, function, 1)
+        f = difference_quotient(xi, function, 1)
+        print(f)
+        return f
     
     return (1 / (xk - xi)) * (((x - xi) * _aitken_neville(i + 1, k, function, x, nodes) - 
                 (x - xk) * _aitken_neville(i, k - 1, function, x, nodes)))
     
 
 def test():
-    nodes = [0.2, 0.4, 0.6] #np.arange(0, 1, 0.2)
+    nodes = [0.2, 0.4] #np.arange(0, 1, 0.2)
     print(nodes)
+    print(aitken_neville(0, math.log, nodes))
+    nodes.append(0.6)
     print(aitken_neville(0, math.log, nodes))
 
 test()
