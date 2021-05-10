dbb324dc159ef8f6c630a9230592a9f314a15d09
Hayri Bakici <hayri.bakici@gmail.com>
2021-05-10 22:49:02 +0200

    adds better print outputs

--- ha_2/run_2_3.py
+++ ha_2/run_2_3.py
@@ -8,28 +8,29 @@ def difference_quotient(h, function, x):
     return (function(x + h) - function(x)) / h
 
 
-def aitken_neville(x, function, nodes):
+def aitken_neville(x, x_, function, nodes):
     n = len(nodes) - 1
-    return _aitken_neville(0, n, function, x, nodes)
+    return _aitken_neville(0, n, x, x_, function, nodes)
 
-def _aitken_neville(i, k, function, x, nodes):
+def _aitken_neville(i, k, x, x_, function, nodes):
     xi = nodes[i]
     xk = nodes[k]
-    print("i: %s, k: %s, xi: %s, xk: %s", i, k, xi, xk)
-    if i == k:
-        f = difference_quotient(xi, function, 1)
-        print(f)
-        return f
+    print("p_", i, "_", k, " = 1 / (", xk, "-", xi, ") * ((", x, "-", xi, ") * p_", i + 1, "_", k, " - (", x, " - ", xk, ") * p_", i, "_", k - 1, ")")  
     
-    return (1 / (xk - xi)) * (((x - xi) * _aitken_neville(i + 1, k, function, x, nodes) - 
-                (x - xk) * _aitken_neville(i, k - 1, function, x, nodes)))
+    if i == k:
+        dx = difference_quotient(xi, function, x_)
+        print(i, " == ", k, " d/dx = ", dx)
+        return dx   
+    
+    return (1 / (xk - xi)) * (((x - xi) * _aitken_neville(i + 1, k, x, x_, function, nodes)) - 
+                ((x - xk) * _aitken_neville(i, k - 1, x, x_, function, nodes)))
     
 
 def test():
-    nodes = [0.2, 0.4] #np.arange(0, 1, 0.2)
-    print(nodes)
-    print(aitken_neville(0, math.log, nodes))
-    nodes.append(0.6)
-    print(aitken_neville(0, math.log, nodes))
+    nodes = [0.2, 0.4, 0.6] #np.arange(0, 1, 0.2)
+    print(aitken_neville(0, 1, math.log, nodes))
+    # TODO what to when we add another node?
+    # nodes.append(0.6)
+    # print(_aitken_neville(len(nodes) - 2, len(nodes) - 1, math.log, 0, nodes))
 
 test()
