d32ffa4909c4c70ded27db41ca58c12df5f8c3fd
Hayri Bakici <hayri.bakici@gmail.com>
2021-05-11 23:11:13 +0200

    first attempt to put it together

--- ha_2/run_2_3.py
+++ ha_2/run_2_3.py
@@ -29,10 +29,17 @@ class AitkenNeville():
         return (self._function(self._xs + h) - self._function(self._xs)) / h
 
     def add_nodes(self, nodes):
+        """
+        Adds a collection of nodes to calculate its interpolation and fills its values
+        into C{results}.
+        @param nodes: the nodes to add.
+
+        @see get_polynome(n) to get the n-th value
+        """
         lenBefore = len(self._nodes)
         self._nodes.extend(nodes)
 
-        self._aitken(lenBefore)
+        self.__aitken(lenBefore)
 
     def add_node(self, node):
         """
@@ -40,6 +47,7 @@ class AitkenNeville():
         into C{results}.
         @param node: the node to add. Must be the smallest number. 
         
+        @see get_polynome(n) to get the n-th value
         """
         # Checking for emptyness and out new value fulfills the requirement
         # as being the smallest item
@@ -49,10 +57,13 @@ class AitkenNeville():
         lenBefore = len(self._nodes)
         
         self._nodes.append(node)
-        self._aitken(lenBefore)
+        self.__aitken(lenBefore)
 
        
-    def _aitken(self, lenBefore):
+    def __aitken(self, lenBefore):
+        """
+        Calculates the aitken-neville algorithm.
+        """
         nodes = self._nodes
         results = self._results
         x = self._x
@@ -66,7 +77,7 @@ class AitkenNeville():
         #   4: [p44, p34, p24, p14, p04], 
         #   5: ...
         # }
-        # Adding another node uses the already existing calculations.
+        # Adding another node(s) uses the already existing calculations.
         for k in range(lenBefore, len(self._nodes)):
             results[k] = []
             # we count backwards: from k to 0.
@@ -83,31 +94,90 @@ class AitkenNeville():
                 # using the "p_i(k-1) - th polynome"
                 p2 = (x - xk) * results[k - 1][(len(results[k - 1]) - 1) - i]
 
-                # putting everythin together
+                # putting everything together
                 results[k].append(frac * (p1 - p2))
 
 
     def get_polynome(self, n): 
         """
         @param n: the n-th polynome to retrieve.
-        @return ... TODO
+        @return the n-th polynome that was calculated with C{add_node} or C{add_nodes} 
+            or C{0} when the polynome was not found.
         """
+        if not n in self._results:
+            return 0
         polynome = len(self._results[n]) - 1
         return self._results[n][polynome]
 
-def f_3(x): 
-    gamma = 10000
-    return (1 / gamma) * math.atan(gamma * x)
 
-def test(n):
-    x_ = 0
-    H = math.pi / 2
+class Test():
 
-    
-    nodes_1 = _get_supporting_points(n, lambda k: 2**(-k) * H)
-    nodes_2 = _get_supporting_points(n, lambda k: H / (k + 1)) # have length of n
+    def __init__(self, n):
+        self._n = n
+        self._x = 0
+        self._H = math.pi / 2
+        self._functions = [lambda x: 1 + math.sin(x), lambda x: math.sqrt(x**3), self.f_3]
+        self._derived_functions = [lambda x: math.cos(x), lambda x: 1.5 * math.sqrt(x), lambda x: 1/(100000000* x**2 + 1)]
+
+    def f_3(self, x): 
+        gamma = 10000
+        return (1 / gamma) * math.atan(gamma * x)
+
+    def calculate(self):
+        n = self._n
+        x_ = self._x
+        H = self._H
+        
+
+        for i in range(1, n + 1):
+            nodes = self.__get_supporting_points(i, )
+            self.__calculate(i, )
+
+    def __calculate(self, i, nodes):
+        functions = self._functions
+        derived_functions = self._derived_functions
+
+        hn = self.__hn(nodes)
+        for f in functions:
+            neville = AitkenNeville(f, 0, 0)
+            neville.add_nodes(nodes)
+                
+            dx = neville.difference_quotient(hn)
 
 
+
+            pn = neville.get_polynome(i)
+
+
+    def __get_supporting_points(self, n, function): 
+        """
+        @param n: the number of nodes to return
+        @param function: a function to apply for each k = 0, ..., n
+
+        @rtype: list
+        @return C{function} applied for each k = 0, ..., n
+        """
+        return [function(k) for k in range(n + 1)]
+
+    def __hn(self, nodes): 
+        """
+        @return the last element of C{nodes}
+        @rtype: float
+        """
+        return nodes[len(nodes) - 1]
+
+    def __get_error(self, derived_function, x, value):
+        """
+        Calculates the error: |f'(x) - value|
+        @param derived_function: the derived function
+        @param x: the position for the derived function to calculate
+        @type x: float
+        @param value: the value to calculate the error.
+
+        @return the error between C{derived_function} and C{value}
+        @rtype: float
+        """
+        return abs(derived_function(x) - value)
 
     # 1. f'(x)
     a = AitkenNeville(lambda x: 1 + math.sin(x), 0, 0)
@@ -118,23 +188,6 @@ def test(n):
     error_w_extra = abs(math.cos(x_) - pn)
     print(error_wo_extra)
     print(error_w_extra)
-
-
-    # n = 1, ..., 40
-    # hk = 2^-k * pi / 2, k = 0, ..., n
-    # for i in range(n):    
-
-        # a = AitkenNeville(lambda x: 1 + math.sin(x), 0, 0)
-        
-        # b = AitkenNeville(lambda x: math.sqrt(x**3), 0, 0)
-
-        # c = AitkenNeville(f_3, 0, 0)
     
-def _get_supporting_points(n, function):
-    mylist = []
-    for k in range(n + 1): # 0, ..., n
-        mylist.append(function(k))
-
-    return mylist
 
 test(n=40)
