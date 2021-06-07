1f51f4dd8d8a3798305dda8c2ab978f323d9cbcf
Hayri Bakici <hayri.bakici@gmail.com>
2021-06-07 21:01:34 +0200

    Adds Umlaute back

--- ha_6/run_6_3.py
+++ ha_6/run_6_3.py
@@ -4,23 +4,23 @@ import math
 import numpy as np
 
 T = 10
-# zum Zeichnen einer schoeneren
+# zum Zeichnen einer schöneren
 T_ = 100*T
 # Startwerte: 
 # x_0 = 1 und x~_0 = 1001
 
 # Generieren der x-Werte
 xvalues = np.linspace(0, T, T_)
-# Berechnung der y-Werte fuer x_0 und x~_0 fuer f(x) = e^(2t) * (x_0 + t^2)
+# Berechnung der y-Werte für x_0 und x~_0 fuer f(x) = e^(2t) * (x_0 + t^2)
 yvalues01 = list(map(lambda t: math.exp(2 * t) * (1 + t**2), xvalues))
 yvalues02 = list(map(lambda t: math.exp(2 * t) * (1001 + t**2), xvalues))
 
-# Berechnung der y-Werte fuer den Fehler |x(t) - x~(t)| 
+# Berechnung der y-Werte für den Fehler |x(t) - x~(t)| 
 error = list(map(lambda x, y: abs(x - y), yvalues01, yvalues02))
 
 plt.figure('Aufgabe 3b): e^(2t) * (x_0 + t^2)')
 plt.subplot(2,1,1)
-# Grafen fuer x und x~
+# Grafen für x und x~
 plt.loglog(xvalues, yvalues01, 'b', label='x_0 = 1')
 plt.loglog(xvalues, yvalues02, 'r', label='x~_0 = 1001')
 
@@ -29,7 +29,7 @@ plt.xlabel('Anzahl t-Werte = %i (logarithmische Skala)' % T_)
 plt.ylabel('x(t) \n(logarithmische Skala)')
 
 plt.subplot(2,1,2)
-# Graf fuer |x(t) - x~(t)|
+# Graf für |x(t) - x~(t)|
 plt.loglog(xvalues, error, label='|x(t) - x~(t)|')
 
 plt.legend()
