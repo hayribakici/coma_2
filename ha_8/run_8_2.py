afb02fa9139b318a1b329fe3540e436f00d51948
Hayri Bakici <hayri.bakici@gmail.com>
2021-06-24 23:55:35 +0200

    adds implementation

--- ha_8/run_8_2.py
+++ ha_8/run_8_2.py
@@ -1,6 +1,24 @@
-# TODO implement me
-
+import numpy as np
 
 # 2b
 def taylor2(mu, x0, T, tau):
-    pass
+
+    n = 10
+    return __taylor2(mu, x0, T, tau, n - 1)
+
+def __taylor2(mu, x0, T, tau, k):
+    
+    if k <= 0:
+        return x0
+    
+    return __taylor2(mu, x0, T, tau, k - 1) - (tau * mu + 1) * __taylor2(mu, x0, T, tau, k - 2)
+    
+
+
+mus = [-1, -2, -3]
+T = 1
+x0 = 1
+tau = 999999999999
+
+for mu in mus:
+    print("τ = ", tau, "μ =", mu, ":", taylor2(mu, x0, T, tau))
